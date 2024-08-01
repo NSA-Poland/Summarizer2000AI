cc9fd8e2a06fee2ad904bdb6e04f5c12b45be3c3
Szymon Scibik <scibik.szym@gmail.com>
2024-08-01 13:52:35 +0200

    add kernel

--- src/web/WebApi/Program.cs
+++ src/web/WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -12,6 +16,17 @@ app.UseHttpsRedirection();
 
 app.MapGet("/api/weatherforecast", () =>
 {
+    var kernelBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(
+        deploymentName: "summarizer2000ai",
+        endpoint: "https://summarizer2000ai.openai.azure.com/",
+        apiKey: "aacf442553ac43be9dace87e93a94f96"
+        );
+    var kernel = kernelBuilder.Build();
+
+    var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+    // Add a plugin (the LightsPlugin class is defined below)
+    //kernel.Plugins.AddFromType<LightsPlugin>("Lights");
     return Results.Ok();
 })
 .WithName("GetWeatherForecast")
