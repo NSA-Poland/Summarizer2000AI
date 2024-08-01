7bd7b874610a4fb17ff176ce831bf65125d41b41
Lukasz Wielopolski <lukasz.wielopolski@outlook.com>
2024-08-01 14:58:45 +0200

    add OpenAIClient

--- src/web/WebApi/Program.cs
+++ src/web/WebApi/Program.cs
@@ -1,3 +1,8 @@
+using Azure.AI.OpenAI;
+using Azure;
+using Azure.Core;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
@@ -14,7 +19,7 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/weatherforecast", () =>
+app.MapGet("/api/weatherforecast", async () =>
 {
     var kernelBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(
         deploymentName: "summarizer2000ai",
@@ -25,9 +30,38 @@ app.MapGet("/api/weatherforecast", () =>
 
     var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
+    var prompt = @"
+    You are an intelligent assistant that summarizes text. Summarize the following input text in a concise and informative manner, highlighting the main points:
+ 
+    Input:
+    {{input}}
+ 
+    Summary:
+    "
+    ;
+
+    var text = await File.ReadAllTextAsync("Sample.txt");
+
+    var client = new OpenAIClient(new Uri("https://summarizer2000ai.openai.azure.com/"), new AzureKeyCredential("aacf442553ac43be9dace87e93a94f96"));
+
+    // Create the request
+    var opt = new CompletionsOptions("summarizer2000ai", [prompt.Replace("{{input}}", text)]);
+    opt.MaxTokens  = 100;
+
+    var summaryResult = await client.GetCompletionsAsync(opt);
+
+
+
+    //var result = await kernel.InvokeAsync(summarize, new() { ["input"] = text });
+
     // Add a plugin (the LightsPlugin class is defined below)
     //kernel.Plugins.AddFromType<LightsPlugin>("Lights");
-    return Results.Ok();
+
+    //var request = new CompletionRequest(prompt, new { input = inputText });
+    //var result = await _kernel.Completions.CreateCompletionAsync(request);
+
+
+    return Results.Ok(summaryResult.Value.Choices[0].Text.Trim());
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
